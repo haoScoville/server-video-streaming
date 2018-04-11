4a7a8192895d24eaec5f414edd657de357bbbe88
Hao <hacaohao@gmail.com>
2018-04-12 00:38:11 +0700

    Refactor project's structure and add login route

--- server.js
+++ server.js
@@ -5,17 +5,21 @@ var Glue=require('glue');
 
 // Start the server
 async function start() {
+    let options = {
+        relativeTo: __dirname + '/lib/plugins'
+    };
 
     try {
-        let server = await Glue.compose(require('./config/manifest.json'))
+        let server = await Glue.compose(require('./config/manifest.json'), options)
         await server.start();
+
+        console.log('Server running at:', server.info.uri);
     }
     catch (err) {
         console.log(err);
         process.exit(1);
     }
-
-    console.log('Server running at:', server.info.uri);
+    
 };
 
 start();
